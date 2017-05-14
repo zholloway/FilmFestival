6ac27dca6f9a1a97abe6222d49d2431823c3e6aa
zholloway <zholloway92@gmail.com>
2017-05-14 17:54:11 -0400

    first commit

---
+++ FilmFestival/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilmFestival.Startup))]
+namespace FilmFestival
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

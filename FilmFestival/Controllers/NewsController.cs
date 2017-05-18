63f1461236a732fe0e555a622a6a2bfaa4ecc579
Zack <zholloway92@gmail.com>
2017-05-18 11:22:56 -0400

    news story page complete

---
+++ FilmFestival/Controllers/NewsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmFestival.Controllers
+{
+    public class NewsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Story()
+        {
+            return View();
+        }
+    }
+}
